1568efeb80085e6ecd11eb9079877189d6086540
Hannes0_0 <hannes.scheibelauer@sz-ybbs.ac.at>
2023-02-05 10:33:58 +0100

    fragen.json + ersten 10 fragen + animation

--- src/pages/questions.tsx
+++ src/pages/questions.tsx
@@ -4,28 +4,25 @@ import Link from "next/link";
 import Image from "next/image";
 import Nav from "./nav";
 import OwnFooter from "./ownFooter";
+const fragen: {
+  [key: string]: { [key: string]: { a: string; p: number }[] };
+} = require("../../public/fragen.json");
 
 export default function Questions() {
-  const qArray = [
-    [],
-    [
-      "Ich trinke nach der Schule meinen Kaffee bei Starbucks",
-      "Ich mache mir daheim einen Kaffe mit der Kaffe Maschine",
-      5,
-      1,
-    ],
-    [
-      "Ich verzichte auf das Rauchen",
-      "Ich kaufe mir nach der Schule Zigaretten",
-      0,
-      7,
-    ],
-  ];
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(-1);
+  const [money, setMoney] = useState(100);
   const [front, setFront] = useState(1);
+  const [moneyMoving, setMoneyMoving] = useState(false);
+  const [moneyMovingValue, setMoneyMovingValue] = useState(0);
+  let vorzeichen = "";
+  if (moneyMovingValue > 0) {
+    vorzeichen = "+";
+  }
+  //console.log(fragen[1][0][0]["a"]); frage[fragenid][welchefragen][1.option][antwort]
+  console.log(fragen[0][0][0]["p"]);
   let objToRender = <></>;
 
-  if (count == 0) {
+  if (count == -1) {
     objToRender = (
       <div className={qStyles.tutorialWrapper}>
         <div className={qStyles.tutorial}>
@@ -35,20 +32,54 @@ export default function Questions() {
           entscheidung treffen wie du dein Geld ausgibst. Nicht immer ist die
           option wo man im moment am meisten geld behält die beste ;)
         </div>
-        <button className={qStyles.startGame} onClick={() => setCount(1)}>
+        <button className={qStyles.startGame} onClick={() => setCount(0)}>
           Starten
         </button>
       </div>
     );
-  } else if (count > 0) {
+  } else if (count > -1) {
     objToRender = (
       <div className={qStyles.Wrapper}>
         <div className={qStyles.moneyDisplayParent}>
-          <div className={qStyles.moneyDisplay}>100€</div>
+          <div className={qStyles.dayDisplay}>
+            <span className={qStyles.greenSpan}>{count + 1}</span>
+          </div>
+          <div className={qStyles.moneyDisplay}>
+            {money}€ {moneyMoving}
+            <div
+              className={
+                qStyles.addmoney +
+                " " +
+                (moneyMoving ? qStyles.moving : qStyles.addmoneyInvisible) +
+                " " +
+                (moneyMovingValue > 0 ? qStyles.green : qStyles.red)
+              }
+            >
+              {vorzeichen}
+              {moneyMovingValue}
+            </div>
+          </div>
         </div>
 
         <div className={qStyles.questions}>
           <button
+            onClick={() => {
+              setMoneyMovingValue(-fragen[count][0][0]["p"]);
+              setMoneyMoving(true);
+              setTimeout(() => {
+                setMoney(money - fragen[count][0][0]["p"]);
+                setMoneyMoving(false);
+              }, 500);
+              setTimeout(() => {
+                setMoneyMovingValue(10);
+                setMoneyMoving(true);
+              }, 1000);
+              setTimeout(() => {
+                setMoneyMoving(false);
+                setCount(count + 1);
+                setMoney(money - fragen[count][0][0]["p"] + 10);
+              }, 1500);
+            }}
             className={
               qStyles.question1Parent +
               " " +
@@ -58,10 +89,27 @@ export default function Questions() {
             }
             onMouseOver={() => setFront(2)}
           >
-            <div className={qStyles.question1}>{qArray[count][0]}</div>
-            <div className={qStyles.price}>{qArray[count][2]}€</div>
+            <div className={qStyles.question1}>{fragen[count][0][0]["a"]}</div>
+            <div className={qStyles.price}>{fragen[count][0][0]["p"]}€</div>
           </button>
           <button
+            onClick={() => {
+              setMoneyMovingValue(-fragen[count][0][1]["p"]);
+              setMoneyMoving(true);
+              setTimeout(() => {
+                setMoney(money - fragen[count][0][1]["p"]);
+                setMoneyMoving(false);
+              }, 500);
+              setTimeout(() => {
+                setMoneyMovingValue(10);
+                setMoneyMoving(true);
+              }, 1000);
+              setTimeout(() => {
+                setMoneyMoving(false);
+                setCount(count + 1);
+                setMoney(money - fragen[count][0][1]["p"] + 10);
+              }, 1500);
+            }}
             className={
               qStyles.question2Parent +
               " " +
@@ -71,8 +119,8 @@ export default function Questions() {
             }
             onMouseOver={() => setFront(1)}
           >
-            <div className={qStyles.question2}>{qArray[count][1]}</div>
-            <div className={qStyles.price}>{qArray[count][3]}€</div>
+            <div className={qStyles.question2}>{fragen[count][0][1]["a"]}</div>
+            <div className={qStyles.price}>{fragen[count][0][1]["p"]}€</div>
           </button>
         </div>
         <OwnFooter></OwnFooter>
